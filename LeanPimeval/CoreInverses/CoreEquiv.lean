0d6c7f6b6b47417d2da43bd3db0456d8c2401bbd
Ethan Ermovick <keg9ve@virginia.edu>
2026-03-17 00:52:31 -0400

    update to use N+ instead of proof arguments

--- LeanPimeval/CoreInverses/CoreEquiv.lean
+++ LeanPimeval/CoreInverses/CoreEquiv.lean
@@ -5,26 +5,24 @@
 -- See the LICENSE file in the root of this repository for more details.
 
 import Mathlib.Logic.Equiv.Defs
+import Mathlib.Data.PNat.Basic
 import LeanPimeval.CoreInverses.Defs
 import LeanPimeval.CoreInverses.Proofs
 
 /-- A single Equiv parametrized by IsBank -/
-def coreEquiv (NumCoreInNextLevel NumBankPerRank NumChipPerRank : Nat) (IsBank : Bool)
-  (h_NumCoreInNextLevel_pos : NumCoreInNextLevel > 0)
-  (h_NumChipPerRank_pos : NumChipPerRank > 0)
-  (h_NumBankPerRank_pos : NumBankPerRank > 0)
-  (h_divides : NumBankPerRank % NumChipPerRank = 0) :
-  PimCoreId ≃ { loc : PimCoreLocation // ValidLocation NumCoreInNextLevel NumBankPerRank NumChipPerRank IsBank loc } where
+def coreEquiv (NumCoreInNextLevel NumBankPerRank NumChipPerRank : ℕ+) (IsBank : Bool)
+  (h_divides : NumBankPerRank.val % NumChipPerRank.val = 0) :
+  PimCoreId ≃ { loc : PimCoreLocation // ValidLocation NumCoreInNextLevel.val NumBankPerRank.val NumChipPerRank.val IsBank loc } where
 
   toFun := fun id =>
-    let loc := getCoreLocation id NumCoreInNextLevel NumBankPerRank NumChipPerRank IsBank
-    ⟨loc, coreEquiv_valid_loc id NumCoreInNextLevel NumBankPerRank NumChipPerRank IsBank h_NumCoreInNextLevel_pos h_NumChipPerRank_pos h_NumBankPerRank_pos h_divides⟩
+    let loc := getCoreLocation id NumCoreInNextLevel.val NumBankPerRank.val NumChipPerRank.val IsBank
+    ⟨loc, coreEquiv_valid_loc id NumCoreInNextLevel.val NumBankPerRank.val NumChipPerRank.val IsBank NumCoreInNextLevel.2 NumChipPerRank.2 NumBankPerRank.2 h_divides⟩
 
   invFun := fun ⟨loc, _⟩ =>
-    getCoreId loc NumCoreInNextLevel NumBankPerRank NumChipPerRank
+    getCoreId loc NumCoreInNextLevel.val NumBankPerRank.val NumChipPerRank.val
 
   left_inv := fun id =>
-    coreEquiv_left_inv id NumCoreInNextLevel NumBankPerRank NumChipPerRank IsBank h_divides
+    coreEquiv_left_inv id NumCoreInNextLevel.val NumBankPerRank.val NumChipPerRank.val IsBank h_divides
 
   right_inv := fun ⟨loc, h_valid⟩ =>
-    Subtype.ext (coreEquiv_right_inv NumCoreInNextLevel NumBankPerRank NumChipPerRank IsBank loc h_valid h_NumCoreInNextLevel_pos h_NumChipPerRank_pos h_divides)
+    Subtype.ext (coreEquiv_right_inv NumCoreInNextLevel.val NumBankPerRank.val NumChipPerRank.val IsBank loc h_valid NumCoreInNextLevel.2 NumChipPerRank.2 h_divides)
