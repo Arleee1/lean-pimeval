730ac8f49a53f8cee0e0874f4af0a2f711902026
Ethan Ermovick <keg9ve@virginia.edu>
2026-03-17 20:40:39 -0400

    cleanup main api

--- LeanPimeval/PimevalAPI/Main.lean
+++ LeanPimeval/PimevalAPI/Main.lean
@@ -1,73 +1,60 @@
 -- File: LeanPimeval/PimevalAPI/Main.lean
 -- LeanPimeval - Contract for the Pimeval API
--- Copyright (c) 2026 Ethan Ermovick
+-- Copyright (c) 2026 Ethan Ermovick, William Bradford
 -- This file is licensed under the MIT License.
 -- See the LICENSE file in the root of this repository for more details.
 
-variable {α : Type} [Add α] [Sub α]
-
-structure PimObj (n : Nat) where
+structure PimObj (α : Type) (n : Nat) where
   elems : Vector α n
-
--- this is a comfort thing, if you want to use the
--- above notation, that also works but I wanted to
--- get a quick version out
-structure PimObj' (a' : Type) (n : Nat) where
-  elems : Vector a' n
 
 -- defines a higher order function which assumes map-based iterative
 -- function application for each PIM kernel.
-def PIM.Map2 {a' : Type} (f : a' -> a' -> a') :
-  let PO := PimObj' a' n
+def PIM.Map2 {n : Nat} {α : Type} (f : α -> α -> α) :
+  let PO := PimObj α n
   PO -> PO -> PO :=
-    fun (src1 src2 : PimObj' a' n) =>
+    fun (src1 src2 : PimObj α n) =>
       let elems := src1.elems.zipWith f src2.elems
-      PimObj'.mk elems
+      PimObj.mk elems
 
-def PIM.Fold {a' : Type} (acc : a' -> a' -> a') :
-  a' -> PimObj' a' n -> a' :=
-  fun (init : a') =>
-    fun (src : PimObj' a' n) =>
+def PIM.Fold {n : Nat} {α : Type} (acc : α -> α -> α) :
+  α -> PimObj α n -> α :=
+  fun (init : α) =>
+    fun (src : PimObj α n) =>
       Vector.foldl (acc) (init) src.elems
-      
-def pimAdd {n} (src1 src2 : PimObj (α := α) n) : PimObj (α := α) n :=
-  let elems := src1.elems.zipWith (· + ·) src2.elems
-  PimObj.mk elems
 
-def pimAdd' [Add a'] {n}
-  (src1 src2 : PimObj' a' n) : PimObj' a' n
+def pimAdd {α : Type} [Add α] {n : Nat}
+  (src1 src2 : PimObj α n) : PimObj α n
   := PIM.Map2 (· + ·) src1 src2
 
-def pimSub {n} (src1 src2 : PimObj (α := α) n) : PimObj (α := α) n :=
+def pimSub {α : Type} [Sub α] {n : Nat} (src1 src2 : PimObj (α := α) n) : PimObj (α := α) n :=
   let elems := src1.elems.zipWith (· - ·) src2.elems
   PimObj.mk elems
 
-def pimRedSum [Zero a'] [Add a'] :
-  PimObj' a' n -> a' :=
-  fun (src : PimObj' a' n) =>
+def pimRedSum {α : Type} {n : Nat} [Zero α] [Add α] :
+  PimObj α n -> α :=
+  fun (src : PimObj α n) =>
     PIM.Fold (fun a b => a + b) 0 src
 
-def pimRedSub [Zero a'] [Sub a'] :
-  PimObj' a' n -> a' :=
-  fun (src : PimObj' a' n) =>
+def pimRedSub {α : Type} {n : Nat} [Zero α] [Sub α] :
+  PimObj α n -> α :=
+  fun (src : PimObj α n) =>
     PIM.Fold (fun a b => a - b) 0 src
 
-def pimMul [Mul a'] :
-  PimObj' a' n -> PimObj' a' n -> PimObj' a' n :=
-  fun (src1 src2 : PimObj' a' n) => PIM.Map2 (fun a b => a * b) src1 src2
+def pimMul {α : Type} [Mul α] {n : Nat} :
+  PimObj α n -> PimObj α n -> PimObj α n :=
+  fun (src1 src2 : PimObj α n) => PIM.Map2 (fun a b => a * b) src1 src2
 
-def pimMAC [Mul a'] [Zero a'] [Add a'] :
-  PimObj' a' n -> PimObj' a' n -> a' :=
-  fun (src1 src2 : PimObj' a' n) =>
+def pimMAC {α : Type} [Mul α] [Zero α] [Add α] {n : Nat} :
+  PimObj α n -> PimObj α n -> α :=
+  fun (src1 src2 : PimObj α n) =>
     pimRedSum (pimMul src1 src2)
 
 instance : Add String where
   add := String.append
 
 #eval pimAdd (PimObj.mk ⟨#["d", "d", "d"], rfl⟩) (PimObj.mk ⟨#["d", "d", "d"], rfl⟩)
-#eval pimAdd' (PimObj'.mk ⟨#["d", "d", "d"], rfl⟩) (PimObj'.mk ⟨#["d", "d", "d"], rfl⟩)
 #eval pimSub (PimObj.mk ⟨#[10, 7, 13], rfl⟩) (PimObj.mk ⟨#[4, 5, 6], rfl⟩)
-#eval pimRedSum (PimObj'.mk ⟨#[10, 7, 13], rfl⟩)
-#eval pimRedSub (PimObj'.mk ⟨(#[10, 7, 13] : Array Int), rfl⟩)
-#eval pimMul (PimObj'.mk ⟨(#[10, 7, 13] : Array Int), rfl⟩) (PimObj'.mk ⟨#[4, 5, 6], rfl⟩)
-#eval pimMAC (PimObj'.mk ⟨(#[10, 7, 13] : Array Int), rfl⟩) (PimObj'.mk ⟨#[4, 5, 6], rfl⟩)
+#eval pimRedSum (PimObj.mk ⟨#[10, 7, 13], rfl⟩)
+#eval pimRedSub (PimObj.mk ⟨(#[10, 7, 13] : Array Int), rfl⟩)
+#eval pimMul (PimObj.mk ⟨(#[10, 7, 13] : Array Int), rfl⟩) (PimObj.mk ⟨#[4, 5, 6], rfl⟩)
+#eval pimMAC (PimObj.mk ⟨(#[10, 7, 13] : Array Int), rfl⟩) (PimObj.mk ⟨#[4, 5, 6], rfl⟩)
