6b9b0dfea2556c9bac525bc87db79209fd3fa5a3
Arleee1 <keg9ve@virginia.edu>
2026-03-20 01:19:49 -0400

    styling

--- LeanPimeval/PimevalAPI/Main.lean
+++ LeanPimeval/PimevalAPI/Main.lean
@@ -9,43 +9,61 @@ structure PimObj (α : Type) (n : Nat) where
 
 -- defines a higher order function which assumes map-based iterative
 -- function application for each PIM kernel.
-def PIM.Map2 {n : Nat} {α : Type} (f : α -> α -> α) :
-  let PO := PimObj α n
-  PO -> PO -> PO :=
-    fun (src1 src2 : PimObj α n) =>
-      let elems := src1.elems.zipWith f src2.elems
-      PimObj.mk elems
+def PIM.Map2
+    {α : Type} (f : α -> α -> α)
+    {n : Nat} :
+    let PO := PimObj α n
+    PO -> PO -> PO :=
+  fun (src1 src2 : PimObj α n) =>
+    let elems := src1.elems.zipWith f src2.elems
+    PimObj.mk elems
 
-def PIM.Fold {n : Nat} {α : Type} (acc : α -> α -> α) :
-  α -> PimObj α n -> α :=
+def PIM.Fold
+    {α : Type} (acc : α -> α -> α)
+    {n : Nat} :
+    α -> PimObj α n -> α :=
   fun (init : α) =>
     fun (src : PimObj α n) =>
       Vector.foldl (acc) (init) src.elems
 
-def pimAdd {α : Type} [Add α] {n : Nat}
-  (src1 src2 : PimObj α n) : PimObj α n
-  := PIM.Map2 (· + ·) src1 src2
+def pimAdd
+    {α : Type} [Add α]
+    {n : Nat}
+    (src1 src2 : PimObj α n) : PimObj α n :=
+  PIM.Map2 (· + ·) src1 src2
 
-def pimSub {α : Type} [Sub α] {n : Nat} (src1 src2 : PimObj (α := α) n) : PimObj (α := α) n :=
+def pimSub
+    {α : Type} [Sub α]
+    {n : Nat}
+    (src1 src2 : PimObj (α := α) n) : PimObj (α := α) n :=
   let elems := src1.elems.zipWith (· - ·) src2.elems
   PimObj.mk elems
 
-def pimRedSum {α : Type} {n : Nat} [Zero α] [Add α] :
-  PimObj α n -> α :=
+def pimRedSum
+    {α : Type} [Zero α] [Add α]
+    {n : Nat} :
+    PimObj α n -> α :=
   fun (src : PimObj α n) =>
     PIM.Fold (fun a b => a + b) 0 src
 
-def pimRedSub {α : Type} {n : Nat} [Zero α] [Sub α] :
-  PimObj α n -> α :=
+def pimRedSub
+    {α : Type} [Zero α] [Sub α]
+    {n : Nat} :
+    PimObj α n -> α :=
   fun (src : PimObj α n) =>
     PIM.Fold (fun a b => a - b) 0 src
 
-def pimMul {α : Type} [Mul α] {n : Nat} :
-  PimObj α n -> PimObj α n -> PimObj α n :=
-  fun (src1 src2 : PimObj α n) => PIM.Map2 (fun a b => a * b) src1 src2
+def pimMul
+    {α : Type} [Mul α]
+    {n : Nat} :
+    PimObj α n -> PimObj α n -> PimObj α n :=
+  fun (src1 src2 : PimObj α n) =>
+    PIM.Map2 (fun a b => a * b) src1 src2
 
-def pimMAC {α : Type} [Mul α] [Zero α] [Add α] {n : Nat} :
-  PimObj α n -> PimObj α n -> α :=
+def pimMAC
+    {α : Type} [Mul α] [Zero α] [Add α]
+    {n : Nat} :
+    PimObj α n -> PimObj α n -> α :=
   fun (src1 src2 : PimObj α n) =>
     pimRedSum (pimMul src1 src2)
 
